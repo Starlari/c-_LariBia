f4d40c8921dbca2cb06f3d19a3cc437e0e937fe3
LarissaBeatriz <larissabeatriz880@gmail.com>
2026-03-03 19:39:48 -0300

    atualização 2

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+// primeiro programa em c++ :D
+
+/*indiscutivelmente,a conduta ética, deve ser cobrada não só dos políticos, mas também dos cidadãos tendo em vista que, os próprios políticos só podem ser eleitos se nós ( o povo ) o escolhermos.
+
+entretanto, grande parte da população não tem esse raciocínio, e muitos ainda dizem ter mas não agem de acordo, isso é um problema gravíssimo, tendo em vista que, os políticos que estão governando, são um espelho do povo, já que nós votamos e elegemos ele.
+
+enquanto tivermos esse pensamento ultrapassado, jamais poderemos nos aprimorar, a conduta ética deve ser cobrada por todos e para todos, independentemente de cargo político ou não.
+
+portanto, para podermos viver em uma sociedade mais avançada, mais ética, melhor governada e acima de tudo, com maior companheirismo de nós para e por nós, a conduta ética deverá ser vista como um dever público. (texto aleatório q achei na internet, só pra testar os comentários no c++)
+*/
+
     int main (){
-        std::cout << "Olá, mundo!";
+        std::cout << "Olá, mundo!" << '\n';
+        std::cout << "Olá, Brasil!" << '\n';
         return 0;
     }
