49720f2af33190c70dd191f68c182f1a009e506a
LarissaBeatriz <larissabeatriz880@gmail.com>
2026-09-04 20:18:45 -0300

    backup

---
+++ conversation(conversao).cpp
@@ -0,0 +1,10 @@
+// nesse arquivo, vai ser um arquivo de conversão, q é basicamente substituir um valor de alguma variável por outro
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    
+    return 0;
+}
